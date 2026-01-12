4d9a88c88c42f9b40fc5716d0b15ad168f125a58
Richard Feng <richardfeng101@gmail.com>
2026-01-11 18:17:23 -0800

    resolved save error, adjusted shake values in tree script

--- scenes/objects/trees/tree.gd
+++ scenes/objects/trees/tree.gd
@@ -15,8 +15,8 @@ func _ready() -> void:
 
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
-	material.set_shader_parameter("shake_intensity", 0.5)
-	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity", 1.0)
+	await get_tree().create_timer(0.5).timeout
 	material.set_shader_parameter("shake_intensity", 0.0)
 
 func on_max_damage_reached() -> void:
